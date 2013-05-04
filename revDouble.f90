6ee029c79ac29162af6deff0e09c7aaa039be857
krahim <karim.rahim@gmail.com>
2013-05-04 15:57:03 -0400

    move symlinks

--- revDouble.f90
+++ revDouble.f90
@@ -1,1 +1,14 @@
-../fhelpers/revDouble.f90
+subroutine revDouble(dat, n)
+  integer i, n, ndiv2, nMiP1
+  double precision :: dat(n), tmp
+
+  ndiv2 = n/2
+
+  do i = 1, ndiv2
+     nMiP1 = n - i + 1
+     tmp = dat(i)
+     dat(i) = dat(nMiP1)
+     dat(nMiP1) = tmp
+  end do
+
+end subroutine revDouble
